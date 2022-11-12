75d47808d5b4cccdfcd9cbef6e6aa19337b6878c
RAFAEL-S-S-ALEMIDA <RAFAEL.SANDER@UFU.BR>
2022-11-11 22:15:06 -0300

    add o segundo codigo

---
+++ somaGauss.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float t;
+    int n;
+    //mensagem para o usuario
+    scanf("%d",&n);
+    
+    t = (1.0+n)*(n/2.0);
+    
+    printf("%.0f",t);
+
+return 0;
+}
