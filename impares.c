df1e6509577ba9000abd1d51a5d208e4d9a8cc7e
RAFAEL-S-S-ALEMIDA <rafael.sander@ufu.br>
2022-12-08 00:44:43 -0300

    add o exercicio 2 da lista 2

---
+++ impares.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int menor, maior;
+    printf("Digite dos numenos sendo que o de menor valor primeiro: \n");
+    scanf("%d %d", &menor, &maior);
+
+    
+    
+    void imparess (menor, maior){
+        for (int i = menor; i <= maior; i++){
+            if(i%2 != 0){
+            printf("%d  ", i);
+            }  
+        }
+ 
+    }
+    if(menor >= maior){
+        printf("Ordem dos numeros digitados esta errado!!");
+    }
+    else{
+        imparess(menor, maior);
+    }
+    return 0;
+}
