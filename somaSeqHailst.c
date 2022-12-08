b3befa4e6011690b6976f712b381ebd964db5b88
RAFAEL-S-S-ALEMIDA <rafael.sander@ufu.br>
2022-12-08 18:04:56 -0300

    add exercicio 6 da lista 2

--- somaSeqHailst.c
+++ somaSeqHailst.c
@@ -2,5 +2,22 @@
 
 int main(){
 
+    int numero;
+    printf("Digite um numero inteiro positivo:\n");
+    scanf("%d", &numero);
+
+    int x = numero;
+    while(numero != 1 ){
+        if(numero%2 == 0){
+            numero = numero/2;
+            x += numero;
+        }
+        else{
+            numero = (3*numero)+1;
+            x += numero;
+        }
+    }
+    printf("%d", x);
+
     return 0;
 }
